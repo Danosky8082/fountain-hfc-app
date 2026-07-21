2a23749a5f9b861ad35d05316c9cc1331bca1e26
Danosky8082 <dcmitch2000ng@gmail.com>
2026-07-21 16:10:54 +0100

    Fix prisma.js: use require and remove custom output

--- prisma.js
+++ prisma.js
@@ -1,3 +1,10 @@
 const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
+const { PrismaPg } = require('@prisma/adapter-pg');
+const { Pool } = require('pg');
+require('dotenv').config();
+
+const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+const adapter = new PrismaPg(pool);
+const prisma = new PrismaClient({ adapter });
+
 module.exports = prisma;

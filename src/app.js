878e030566e72e73b275115c8687c2da25a99517
Danosky8082 <dcmitch2000ng@gmail.com>
2026-07-21 17:01:13 +0100

    update the app

--- src/app.js
+++ src/app.js
@@ -9,6 +9,10 @@ require('dotenv').config();
 
 const app = express();
 
+app.get('/hello', (req, res) => {
+  res.send('Hello World');
+});
+
 // Middleware
 app.use(cors());
 app.use(express.json());

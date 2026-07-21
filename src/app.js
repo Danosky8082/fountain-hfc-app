b738593e1522cfabdad50fbe22ce13d1475c7ba8
Danosky8082 <dcmitch2000ng@gmail.com>
2026-07-21 16:56:20 +0100

    update the app

--- src/app.js
+++ src/app.js
@@ -12,6 +12,10 @@ const app = express();
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use((req, res, next) => {
+  console.log(`📥 ${req.method} ${req.url}`);
+  next();
+});
 
 // Routes
 const authRoutes = require('./routes/authRoutes');
